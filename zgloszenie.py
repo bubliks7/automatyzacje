403247251a5aec5d449ed8838b36b4bf14ba0dba
Jakub <bubliksiur@gmail.com>
2025-12-01 22:08:51 +0100

    my first mini-automati

---
+++ zgloszenie.py
@@ -0,0 +1,11 @@
+użytkownicy = [
+    {"name": "Adam", "status": "active"},
+    {"name": "Ewa", "status": "locked"},
+    {"name": "Ola", "status": "active"},
+]
+
+for user in użytkownicy:
+    if user["status"] == "active":
+        print('Uzytkownik ' + user['name'] + ' jest aktywny')
+    else:
+        print('Uzytkownik ' + user['name'] + ' jest zablokowany - zgłaszam do systemu')
