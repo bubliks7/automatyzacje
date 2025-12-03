1f2fa3c190a55d708edc773dc835e93485063fc9
Bubliks <149240768+bubliks7@users.noreply.github.com>
2025-12-03 21:47:08 +0100

    Update zgloszenie.py

--- zgloszenie.py
+++ zgloszenie.py
@@ -1,7 +1,7 @@
 użytkownicy = [
     {"name": "Adam", "status": "active"},
     {"name": "Ewa", "status": "locked"},
-    {"name": "Ola", "status": "active"},
+    {"name": "Ola", "status": "active"}
 ]
 
 for user in użytkownicy:
@@ -9,3 +9,4 @@ for user in użytkownicy:
         print('Uzytkownik ' + user['name'] + ' jest aktywny')
     else:
         print('Uzytkownik ' + user['name'] + ' jest zablokowany - zgłaszam do systemu')
+        
